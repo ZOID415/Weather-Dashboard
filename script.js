bac02327555bf9a368617e13d86fc5fbcfc7f2a1
ZOID415 <michaelalfaro93@gmail.com>
2023-01-25 19:41:14 -0800

    added container, divs, and displayed data for wind, temp, and humidity for 5 days including current day

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function weatherLookup(event) {
+const weatherLookup = function (event) {
   event.preventDefault();
 
   var city = $("#city").val();
@@ -12,24 +12,71 @@ function weatherLookup(event) {
     })
     .then(function (data) {
       console.log(data);
-      forecastLookup(data.coord.lat, data.coord.lon)
+      current(data);
+      forecastLookup(data.coord.lat, data.coord.lon);
+
+      var container = $("#current");
       //data in console will have lat and long for city
       //then do string concat on line 17 replacing lat and long with what you get in the data object
       //fetch("https//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7e8f7106e0004f7fac5f624653ef7dca")
     });
 
   //fetch("https//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=7e8f7106e0004f7fac5f624653ef7dca")
-}
-function forecastLookup(lat, lon) {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=7e8f7106e0004f7fac5f624653ef7dca&units=imperial")
-    .then(function (response){
-       console.log(response)
-       return response.json()
-    }).then(function(data){
-        console.log(data.list)
-        var daysArray = [data.list[0], data.list[8], data.list[16], data.list[24], data.list[32]]
-        console.log(daysArray)
-        
+};
+const forecastLookup = function (lat, lon) {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=7e8f7106e0004f7fac5f624653ef7dca&units=imperial"
+  )
+    .then(function (response) {
+      //console.log(response)
+      return response.json();
     })
-}
-$("#city-search").on("submit", weatherLookup)
+    .then(function (data) {
+      console.log(data.list)
+      forecast(data);
+    });
+};
+const current = function (data) {
+  $("#name").text(data.name);
+  $("#tempurature").text(data.main.temp);
+  $("#wind").text(data.wind.speed);
+  $("#humidity").text(data.main.humidity);
+  saveSearch(data.name);
+  console.log(data);
+};
+
+const forecast = function (data) {
+  var daysArray = [
+    data.list[0],
+    data.list[8],
+    data.list[16],
+    data.list[24],
+    data.list[32],
+  ];
+  var template = $(".forecast");
+  daysArray.forEach(function (day) {
+    var card = $("<div>", { class: "card" });
+
+    var wind = $("<p>");
+    var temp = $("<p>");
+    var humidity = $("<p>");
+    wind.text(day.wind.speed + "MPH");
+    humidity.text(day.main.humidity + "%");
+    temp.text(day.main.feels_like + "F");
+    card.appendTo(template);
+    card.append(wind, temp, humidity);
+    template.append(card);
+  });
+};
+//To do: format in css then add funcionality to bottom two functions, then format butttons 
+const loadSearch = function () {
+  //To Do: Load local storage and append to the screen
+};
+const saveSearch = function (city) {
+  //To Do: Load local storage and add to the list
+};
+$("#city-search").on("submit", weatherLookup);
